78b09cdb4561cf61fa266bda0cf7b586fdc585e5
safdarmustafa <razasaif276@gmail.com>
2026-07-17 16:10:22 +0530

    bump version to 1.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.squarenova.emaanwallpapers"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.1.0"
+        versionCode = 6
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
